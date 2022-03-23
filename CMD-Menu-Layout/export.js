75f478f182ca67129f329f456d14d9657c2d8ea1
CyberMafiya <102012402+CyberMafiya@users.noreply.github.com>
2022-03-23 10:29:26 +0530

    Create export.js

---
+++ CMD-Menu-Layout/export.js
@@ -0,0 +1,30 @@
+let Details = require('sew-queen-pro/sources/dc/Details');
+let Main = require('./Menus/_Default')
+var DOWNLOADCMD = Main.DOWNLOADCMD;var LOGOCMD = Main.LOGOCMD;var PROFILECMD = Main.PROFILECMD;var ADMINCMD = Main.ADMINCMD;var AUDIOCMD = Main.AUDIOCMD;var VIDEOCMD = Main.VIDEOCMD;var IMAGECMD = Main.IMAGECMD;var DEEPAICMD = Main.DEEPAICMD
+
+
+
+// example your layout 👇👇
+// let yourlayout  = require('./Menus/yourfilename')
+
+// exsample for add your own layout
+/*
+
+if(Details.CMD_LAYOUT == 'yourlayoutname') {
+
+ DOWNLOADCMD = yourlayout.DOWNLOADCMD
+ LOGOCMD = yourlayout.LOGOCMD
+ PROFILECMD = yourlayout.PROFILECMD
+ ADMINCMD = yourlayout.ADMINCMD
+ AUDIOCMD = yourlayout.AUDIOCMD
+ VIDEOCMD = yourlayout.VIDEOCMD
+ IMAGECMD = yourlayout.IMAGECMD
+ DEEPAICMD = yourlayout.DEEPAICMD
+ 
+}
+
+*/
+
+
+
+module.exports = {DOWNLOADCMD: DOWNLOADCMD,LOGOCMD: LOGOCMD,PROFILECMD: PROFILECMD,ADMINCMD: ADMINCMD,AUDIOCMD: AUDIOCMD,VIDEOCMD: VIDEOCMD,IMAGECMD: IMAGECMD,DEEPAICMD: DEEPAICMD}
