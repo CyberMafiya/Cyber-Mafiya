a60557c2e0d12854c75f261d24e1f0c0892f2440
CyberMafiya <102012402+CyberMafiya@users.noreply.github.com>
2022-03-27 09:23:08 +0530

    Delete plugins directory

--- plugins/AMAZONE.py
+++
@@ -1,7 +0,0 @@
-/* Copyright (C) 2021 TENUX-Neotro.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
-*/
-
-481c863d-be29-4b7f-854c-ea859ea2dacff281ff67-7b39-470a-b7b0-12dc933d8c11481c863d-be29-4b7f-854c-ea859ea2dacff281ff67-7b39-470a-b7b0-12dc933d8c11481c863d-be29-4b7f-854c-ea859ea2dacff281ff67-7b39-470a-b7b0-12dc933d8c11481c863d-be29-4b7f-854c-ea859ea2dacf
